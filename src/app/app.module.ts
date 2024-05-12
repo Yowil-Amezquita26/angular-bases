348e2315a0864b2928191c4323a81ca8bf3bbdb9
Yowil Amezquita <yowilamezquita380@hotmail.com>
2024-05-12 19:51:16 -0400

    created docs file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
@@ -10,7 +9,12 @@ import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule, DbzModule],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
